07d41fffbe44a1f033c01d4575fed900a4718f56
jacont14 <jacont14@asu.edu>
2023-09-28 14:17:15 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -26,3 +26,24 @@ for(let span of ltBlueSpans){
 for(let span of medBlueSpans){
     span.style.color = "#3C5E73";
 }
+
+// selecting a group with a css selector
+let colors = ["#283040", "#3C5E73", "#7C9EA6", "#D9BCA3", "#F2DCC9"];
+let svgs = document.querySelectorAll("#svgs svg");
+//console.log(svgs);
+// iterate through the svg collections and change the color of each
+
+for(let i = 0; 1 < svgs.length; i++){
+svgs[i].style.stroke = colors[i];
+}
+
+// individual element with css selector
+document.querySelector(".bold").style.color = "#7C9EA6";
+
+// changing dom node content
+document.querySelector(".content_list li:first-of-type").textContent = "this is the new list item text";
+
+document.querySelectorAll(".content_list li")[2].innerHTML += " <strong>Juan Contreras</strong";
+
+//removing attributes
+document.querySelector("#remove a").removeAttribute("hidden");
